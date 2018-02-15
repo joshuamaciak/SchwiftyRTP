2d0e0cd61fd6e0a8cb90d71031a47ff8aec06044
Joshua Maciak <jtm97@pitt.edu>
2018-02-15 14:21:31 -0500

    Initial Commit

---
+++ SchwiftyRTP/SchwiftyRTP.h
@@ -0,0 +1,19 @@
+//
+//  SchwiftyRTP.h
+//  SchwiftyRTP
+//
+//  Created by Joshua Maciak on 2/15/18.
+//  Copyright © 2018 Joshua Maciak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SchwiftyRTP.
+FOUNDATION_EXPORT double SchwiftyRTPVersionNumber;
+
+//! Project version string for SchwiftyRTP.
+FOUNDATION_EXPORT const unsigned char SchwiftyRTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SchwiftyRTP/PublicHeader.h>
+
+
